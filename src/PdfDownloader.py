566c8a57cb8e896cb63b0da92503eda0c92feac6
Jairo <xyos@xyos-laptop.(none)>
2009-12-25 20:55:43 -0500

    se eliminaron facturas del host, deben añadirse a gitignore la carpeta facturas... se añadio la funcion para convertir a xml en pdfdowloader.py

--- src/PdfDownloader.py
+++ src/PdfDownloader.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Esta clase crea un objeto de tipo Thread, que recibe como parametros un valor inicial
-y uno final, itera en esos valores descargando los pdfs que genere la aplicación de telmex
-, se debe instanciar de la siguiente forma:
+y uno final, itera en esos valores descargando los pdfs que genere la aplicación de telmex, 
+en el proceso los convierte a xml, se debe instanciar de la siguiente forma:
 objetox = PdfDownloader(valor1,valor2).start()
 donde valor1 y valor 2 son enteros positivos.
 """
 import httplib
 import threading
+from pdfminer.converter import XMLConverter
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 
 HOST="190.27.201.2"
 URL="/bill_telmex.php?JULIO="
@@ -27,13 +29,21 @@ class PdfDownloader(threading.Thread):
 		self.download(self.ini,self.fin)
 
 
-	def savepdf(self,num,pdffile):
-		filename = DIR+str(num)+".pdf"
-		fileout = file(filename,'w')
+	def savepdf(self,filename,pdffile):
+		fileout = file(filename+".pdf",'w')
 		fileout.write(pdffile)
 		fileout.close
-
-
+	
+	def to_xml(self, filename):
+		src = file(filename+".pdf",'rb')
+		out = file(filename+".xml", 'w')
+		rsrc = PDFResourceManager()
+		converter = XMLConverter(rsrc, out, codec='utf-8', laparams=None)
+		process_pdf(rsrc, converter, src, 0, maxpages=0, password='')
+		src.close
+		out.close
+		converter.close
+		
 	def download(self,ini,fin):
 		for num in xrange(ini,fin):
 			conection = httplib.HTTPConnection(HOST)
@@ -42,6 +52,7 @@ class PdfDownloader(threading.Thread):
 			if response.getheader('content-type') == 'application/pdf':
 				print num
 				pdffile = response.read()
-				self.savepdf(num,pdffile)
+				filename = DIR+str(num)
+				self.savepdf(filename,pdffile)
+				self.to_xml(filename)
 			conection.close
-
